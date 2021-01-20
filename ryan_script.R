23fd9b7f444596f516cb908a04717075eb3fd955
rchan26 <rchan@turing.ac.uk>
2021-01-20 12:01:44 +0000

    data

--- ryan_script.R
+++ ryan_script.R
@@ -6,13 +6,17 @@ variables <- read.csv('Variables.csv')
 # ----- change characters to factors
 data <- data %>% mutate_if(is.character, as.factor)
 
+# ----- columns that have 2 unqiue values should be as a factor
+indicies <- which(sapply(data, function(col) length(unique(col))) == 2)
+data[,indicies] <- as.factor(data[,indicies])
+
 # ----- remove cases that are missing from the INVET variable
 data <- data[complete.cases(data[,"INVET"]),]
 nrow(data)
 
 # ----- remove variables which shouldn't be included in the analysis
 remove_variables <- c("INVID", "AGEU", "BAGEU", "BWTU", "BHTU", "BAGE", 
-                      "AGE40", "AGE60", "AGE65", "AGE70", "AGE75", "X")
+                      "AGE40", "AGE55", "AGE60", "AGE65", "AGE70", "AGE75", "X")
 data <- subset(data, select = -which(colnames(data) %in% remove_variables))
 
 # ------ remove variables that only have one factor or one unique value
@@ -35,4 +39,7 @@ data <- data[,(proportion_of_complete_cases(data) > cut_off_point)]
 plot(proportion_of_complete_cases(data))
 
 write.csv(x = data, file = "amended_data.csv", row.names = FALSE)
+
+
+
 
